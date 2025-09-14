a929c1279757e0728c4e3d3b2285975196e46bd1
Rishu Gupta <rishukumargupta8409@gmail.com>
2025-09-14 23:44:13 +0530

    change updates

---
+++ dict_questions/max_value_key.py
@@ -0,0 +1,20 @@
+'''
+Key with the Highest Value
+
+Define a function which takes a dictionary as a parameter and returns the key with the highest value in a dictionary.
+
+Example:
+
+    my_dict = {'a': 5, 'b': 9, 'c': 2}
+    max_value_key(my_dict))
+
+Output:
+
+b 
+'''
+def max_value_key(my_dict):
+    # TODO
+    return max(my_dict,key=my_dict.get)
+
+my_dict = {'a': 5, 'b': 9, 'c': 2}
+print(max_value_key(my_dict))
