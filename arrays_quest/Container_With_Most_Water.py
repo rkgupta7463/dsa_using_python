4f1299a045b3c8ab617cb7652fbaad56d06d9c06
Rishu Gupta <rishukumargupta8409@gmail.com>
2025-09-15 21:11:06 +0530

    change updates

---
+++ arrays_quest/Container_With_Most_Water.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        left=0
+        right=len(height)-1
+        max_area=0
+
+        for i in range(left,right):
+            ##with
+            w=right-left
+
+            ## height 
+            h=min(height[left],height[right])
+
+            area=w*h
+
+            ## update the max area
+            max_area=max(max_area,area)
+
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+
+        return max_area
+
+
+print(Solution().maxArea(height=[1,1]))
---
+++ arrays_quest/trapping_rain_water.py
@@ -0,0 +1,14 @@
+'''
+1. Trapping Rain Water
+
+Question: Given n non-negative integers representing elevation map, compute how much water it can trap after raining.
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+
+Output: 6
+
+Hint: Use two pointers tracking left_max and right_max. Water = min(left_max, right_max) - height[i].
+'''
+
+def solution(height:list[int]):
+    return
