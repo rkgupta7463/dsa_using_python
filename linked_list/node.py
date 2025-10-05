6a783ea14cf4bd577632682c462f7544ce78c467
Rishu Gupta <rishukumargupta8409@gmail.com>
2025-10-05 22:33:41 +0530

    linked listed code- linked list classs

--- linked_list/node.py
+++ linked_list/node.py
@@ -3,10 +3,36 @@ class Node:
         self.value=value
         self.next=None
 
-    def display(self):
-        return f"{self.value}" 
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.lenght=0
 
-new_node_1=Node(5)
-new_node_2=Node(10)
-new_node_1.next=new_node_1
-print(new_node_2.display())    
+    def __str__(self):
+        temp_node=self.head
+        result=''
+        while temp_node is not None:
+            result+=str(temp_node.value)
+            if temp_node.next is not None:
+                result+=' -> '
+            temp_node=temp_node.next
+
+        return result
+
+    def append(self,value):
+        new_node=Node(value=value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.lenght+=1
+
+new_linked_list=LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(15)
+new_linked_list.append(16)
+print(new_linked_list)    
